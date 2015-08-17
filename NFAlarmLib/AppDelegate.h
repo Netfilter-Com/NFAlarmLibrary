8641fd73aa727841201d8350ac4af69dc551f54e
Pedro Paulo <ppj@netfilter.com.br>
2015-08-17 13:45:24 -0300

    Initial Commit

---
+++ NFAlarmLib/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  NFAlarmLib
+//
+//  Created by Pedro Paulo Oliveira Junior on 17/08/15.
+//  Copyright (c) 2015 Netfilter. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
